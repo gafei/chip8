1c1afe601a77587a58d890b9539dc493f510e2d3
Gafei Szeto <gafei.szeto@gmail.com>
2019-12-26 18:20:35 -0500

    included a main file. Initial chip8 implementation

--- chip8.c
+++ chip8.c
@@ -1,9 +1,16 @@
 #include "chip8cpu.h"
-#include <GLUT/glut.h> // use OpenGL for now. Possibly use Metal in the future?
 
-int main(){
+void chip8::init()
+{
+	//init registers and memory once
+}
 
+void chip::emulateCycle()
+{
 
+	// Fetch Opcode
+	// Decode Opcode
+	// Execute Opcode
 
-	return 0;
+	// Update timers
 }
---
+++ main.c
@@ -0,0 +1,40 @@
+#include "chip8cpu.h"
+#include <GLUT/glut.h> // use OpenGL for now. Possibly use Metal in the future?
+
+
+chip8 myChip8;
+
+int main(int argc, char **argv)
+{
+
+	// init the graphics renderer and register input callbacks
+	//setUpGrahics();
+	//setUpInput();
+
+	//Init the chip8 system and load the game into memory
+
+	myChip8.init();
+	myChip8.loadGame("pong");
+
+	// The actual emulation loop
+
+	for(;;)
+	{
+
+		// Emulate one CPU cycle
+		myChip8.emulateCycle();
+
+		//If the draw flag is set, update the screen
+		if(myChip8.drawFlag)
+		{
+			drawGraphics();
+		}
+
+		// Store keypress state, during press and release
+		myChip8.setKeys();
+
+
+	}
+
+	return 0;
+}
